78af5352da00f1e44464bcfe2f277c2b92bca875
Kang Shend Rong <shendrongddv@gmail.com>
2023-10-25 10:24:01 +0700

    20231025 : Demo Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <>
       <section className="h-screen px-4 py-16">
         <div className="container flex h-full flex-col items-center justify-center gap-6">
-          <h1 className="font-lora text-center text-2xl font-bold md:text-5xl">
+          <h1 className="text-center font-lora text-2xl font-bold md:text-5xl">
             Home
           </h1>
-          <Button>Test Button</Button>
+          <Button asChild>
+            <Link href="/home-6">Home 6</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/home-7">Home 7</Link>
+          </Button>
         </div>
       </section>
     </>
