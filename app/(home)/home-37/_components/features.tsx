e7c57ac90b2eb4d6f2f830bc6a0641c9cb1c8430
Kang Shend Rong <shendrongddv@gmail.com>
2023-10-26 12:54:10 +0700

    20231026 : Footer

--- app/(home)/home-37/_components/features.tsx
+++ app/(home)/home-37/_components/features.tsx
@@ -1,17 +1,29 @@
+import Image from "next/image";
 import { dataFeatures } from "../data";
 
 const SectionFeatures = () => {
   return (
     <section className="px-4 pt-24">
       <div className="container">
-        <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-4">
+        <ul className="grid gap-6 sm:grid-cols-2 md:grid-cols-4">
           {dataFeatures?.map((item) => (
             <li
               key={item.id}
-              className="flex flex-col items-center gap-2 p-4 text-center transition duration-500 hover:bg-white hover:shadow-lg md:p-10"
+              className="flex gap-6 md:flex-col md:items-center md:p-6 md:text-center"
             >
-              <h3 className="text-2xl font-bold text-black">{item.title}</h3>
-              <p>{item.desc}</p>
+              <figure className="shrink-0 pt-1">
+                <Image
+                  src={`/37/${item.media}`}
+                  alt={item.title}
+                  width={50}
+                  height={50}
+                  className="h-12 w-auto"
+                />
+              </figure>
+              <div>
+                <h3 className="text-2xl font-bold text-black">{item.title}</h3>
+                <p>{item.desc}</p>
+              </div>
             </li>
           ))}
         </ul>
