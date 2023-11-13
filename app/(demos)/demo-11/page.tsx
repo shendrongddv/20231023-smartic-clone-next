d0ed218dd17c84fb07667a3b2f8dccbb88615264
Kang Shend Rong <shendrongddv@gmail.com>
2023-11-14 01:11:37 +0700

    20231113: Demo 11: Fix Error

--- app/(demos)/demo-11/page.tsx
+++ app/(demos)/demo-11/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
 import D11_Section_Differentiate from "./_components/d11-differentiate";
 import D11_Section_Reviews from "./_components/d11-reviews";
 import D11_Section_ShopNow from "./_components/d11-shop-now";
 import D11_Section_Point1 from "./_components/d11-point-1";
 import D11_Section_Point2 from "./_components/d11-point-2";
 import D11_Section_Point3 from "./_components/d11-point-3";
-import D11_Section_Test from "./_components/d11-point-test";
 
 const Demo_11_Page = () => {
   return (
